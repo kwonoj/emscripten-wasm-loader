f4a6933fb782845738169b53a7fff3644cca2b97
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-13 23:53:43 -0700

    feat(index): export interfaces

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { ENVIRONMENT } from './environment';
+export { getModuleLoader } from './getModuleLoader';
+export { WASMTYPE } from './wasmType';
+export { enableLogger, log } from './util/logger';
