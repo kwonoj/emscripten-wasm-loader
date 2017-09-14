38ac5f34527d4079bff4d4d5b421b762ddc17f7c
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-14 16:28:26 -0700

    fix(constructmodule): correct node.js locatefile

--- src/constructModule.ts
+++ src/constructModule.ts
@@ -1,20 +1,42 @@
 import { ENVIRONMENT } from './environment';
 
-export const constructModule = (value: { [key: string]: any }, environment: ENVIRONMENT, binaryEndpoint?: string) => {
+/**
+ * Build stringmap object to be injected when creates runtime for asm module.
+ * Modularized asm module generated via MODULARIZE=1 accepts object as its creation function allow to attach
+ * properties. Using those, this function construct few essential convinient functions like awaitable runtime init.
+ *
+ * Note some init like exporting in-memory FS functions can't be achieved via module object but should rely on
+ * preprocessor (https://github.com/kwonoj/docker-hunspell-wasm/blob/eba7781311b31028eefb8eb3e2457d11f294e076/preprocessor.js#L14-L27)
+ * to access function-scope variables inside.
+ * @param {{[key: string]: any}} value pre-constructed value to be used, or empty object {}.
+ * @param {ENVIRONMENT} environment Running environment to determine to use node.js specific path resolve.
+ * @param {string} [asmDir] ABSOLUTE DIR PATH to wasm / asm binary (.wasm, .mem). If not specified, it'll try best guess via __dirname
+ * @param {string} [binaryEndpoint] Provides endpoint to server to download binary module
+ * (.wasm, .mem) via fetch when initialize module in a browser environment.
+ *
+ * @returns {{[key: string]: any}} Augmented object with prefilled interfaces.
+ */
+export const constructModule = (
+  value: { [key: string]: any },
+  environment: ENVIRONMENT,
+  asmDir: string | null,
+  binaryEndpoint?: string
+) => {
   const ret = {
     ...value,
     __asm_module_isInitialized__: false,
     locateFile: (_fileName: string) => {
       /*noop*/
     },
-    initializeRuntime: () => Promise.resolve(true),
     onRuntimeInitialized: () => {
       /*noop*/
-    }
+    },
+    initializeRuntime: () => Promise.resolve(true)
   };
 
-  //if binaryEndpoint provided, consider it as override behavior and set locateFile fn
-  //otherwise set default to locateFile on node.js
+  //If binaryEndpoint provided, consider it as override behavior and set locateFile fn
+  //otherwise set default to locateFile on node.js.
+  //Browser environment doesn't set custom locateFile if binaryEndpoint is not set.
   if (!!binaryEndpoint) {
     ret.locateFile = (fileName: string) =>
       environment === ENVIRONMENT.NODE
@@ -23,7 +45,7 @@ export const constructModule = (value: { [key: string]: any }, environment: ENVI
         : `${binaryEndpoint}/${fileName}`;
   } else if (environment === ENVIRONMENT.NODE) {
     //tslint:disable-next-line:no-require-imports
-    ret.locateFile = (fileName: string) => require('path').join(__dirname, fileName);
+    ret.locateFile = (fileName: string) => require('path').join(asmDir || __dirname, fileName);
   }
 
   //export initializeRuntime interface for awaitable runtime initialization
