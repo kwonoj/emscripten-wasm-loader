75f49cc429763d45866ed3214e03359c829785a4
OJ Kwon <kwon.ohjoong@gmail.com>
2018-07-11 00:18:03 -0700

    test(getmoduleloader): deprecate environment

--- spec/getModuleLoader-spec.ts
+++ spec/getModuleLoader-spec.ts
@@ -1,5 +1,4 @@
 import { expect } from 'chai';
-import { ENVIRONMENT } from '../src/environment';
 import { getModuleLoader } from '../src/getModuleLoader';
 
 jest.mock('../src/util/isNode');
@@ -9,7 +8,7 @@ jest.mock('../src/constructModule');
 const mockIsNode = require('../src/util/isNode').isNode as jest.Mock<any>;
 
 describe('getModuleLoader', () => {
-  it('should create loader function with node environment', async () => {
+  it('should create loader function', async () => {
     mockIsNode.mockReturnValueOnce(true);
 
     const factoryLoader = jest.fn(() => 'boo');
@@ -23,52 +22,8 @@ describe('getModuleLoader', () => {
     expect(runtimeModule.mock.calls).to.have.lengthOf(1);
     expect(factoryLoader.mock.calls).to.have.lengthOf(1);
     expect(factoryLoader.mock.calls[0][0]).to.deep.equal(mockRuntime);
-    expect(factoryLoader.mock.calls[0][1]).to.equal(ENVIRONMENT.NODE);
 
     expect(loaded).to.equal('boo');
-  });
-
-  it('should create loader function with browser environment', async () => {
-    mockIsNode.mockReturnValueOnce(false);
-
-    const factoryLoader = jest.fn(() => 'boo');
-    const mockRuntime = { initializeRuntime: () => Promise.resolve(true) };
-    const runtimeModule = jest.fn(() => mockRuntime);
-
-    const loader = getModuleLoader(factoryLoader as any, runtimeModule);
-
-    const loaded = await loader();
-
-    expect(runtimeModule.mock.calls).to.have.lengthOf(1);
-    expect(factoryLoader.mock.calls).to.have.lengthOf(1);
-    expect(factoryLoader.mock.calls[0][0]).to.deep.equal(mockRuntime);
-    expect(factoryLoader.mock.calls[0][1]).to.equal(ENVIRONMENT.WEB);
-
-    expect(loaded).to.equal('boo');
-  });
-
-  it('should allow override environment to browser via loader', async () => {
-    mockIsNode.mockReturnValueOnce(true);
-    const factoryLoader = jest.fn(() => 'boo');
-    const mockRuntime = { initializeRuntime: () => Promise.resolve(true) };
-    const runtimeModule = jest.fn(() => mockRuntime);
-
-    const loader = getModuleLoader(factoryLoader as any, runtimeModule);
-
-    await loader(ENVIRONMENT.WEB);
-    expect(factoryLoader.mock.calls[0][1]).to.equal(ENVIRONMENT.WEB);
-  });
-
-  it('should allow override environment to node via loader', async () => {
-    mockIsNode.mockReturnValueOnce(false);
-    const factoryLoader = jest.fn(() => 'boo');
-    const mockRuntime = { initializeRuntime: () => Promise.resolve(true) };
-    const runtimeModule = jest.fn(() => mockRuntime);
-
-    const loader = getModuleLoader(factoryLoader as any, runtimeModule);
-
-    await loader(ENVIRONMENT.NODE);
-    expect(factoryLoader.mock.calls[0][1]).to.equal(ENVIRONMENT.NODE);
   });
 
   it('should throw when init runtime fail', async () => {
