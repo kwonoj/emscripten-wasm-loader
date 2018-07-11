8571b22b453625b4ba0c3cafa2c0434a18e7c13b
OJ Kwon <kwon.ohjoong@gmail.com>
2018-07-11 02:02:47 -0700

    test(path): update test cases

---
+++ spec/path/isMounted-spec.ts
@@ -0,0 +1,82 @@
+import { expect } from 'chai';
+import { isMounted } from '../../src/path/isMounted';
+
+const getFsMock = () => ({
+  stat: jest.fn(),
+  isDir: jest.fn(),
+  isFile: jest.fn()
+});
+
+describe('isMounted', () => {
+  it('should not throw if stat fails', () => {
+    const fsMock = getFsMock();
+    fsMock.stat.mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    expect(() => isMounted(fsMock as any, 'dummy', 'dir')).to.not.throw();
+    expect(isMounted(fsMock as any, 'dummy', 'dir')).to.be.false;
+  });
+
+  it('should not log fail if stat throws ENOENT', () => {
+    const fsMock = getFsMock();
+    fsMock.stat.mockImplementationOnce(() => {
+      const e = new Error();
+      (e as any).code = 'ENOENT';
+      throw e;
+    });
+
+    expect(() => isMounted(fsMock as any, 'dummy', 'dir')).to.not.throw();
+    expect(isMounted(fsMock as any, 'dummy', 'dir')).to.be.false;
+  });
+
+  it('should check directory', () => {
+    const fsMock = getFsMock();
+    fsMock.stat.mockReturnValueOnce({});
+    isMounted(fsMock as any, 'dummy', 'dir');
+
+    expect(fsMock.isDir.mock.calls).to.have.lengthOf(1);
+    expect(fsMock.isFile.mock.calls).to.have.lengthOf(0);
+  });
+
+  it('should check file', () => {
+    const fsMock = getFsMock();
+    fsMock.stat.mockReturnValueOnce({});
+    isMounted(fsMock as any, 'dummy', 'file');
+
+    expect(fsMock.isDir.mock.calls).to.have.lengthOf(0);
+    expect(fsMock.isFile.mock.calls).to.have.lengthOf(1);
+  });
+
+  it('should return true when mounted with directory', () => {
+    const fsMock = getFsMock();
+    fsMock.stat.mockReturnValueOnce({});
+    fsMock.isDir.mockReturnValueOnce(true);
+
+    expect(isMounted(fsMock as any, 'dummy', 'dir')).to.be.true;
+  });
+
+  it('should return true when mounted with file', () => {
+    const fsMock = getFsMock();
+    fsMock.stat.mockReturnValueOnce({});
+    fsMock.isFile.mockReturnValueOnce(true);
+
+    expect(isMounted(fsMock as any, 'dummy', 'file')).to.be.true;
+  });
+
+  it('should return false when mounted but not directory', () => {
+    const fsMock = getFsMock();
+    fsMock.stat.mockReturnValueOnce({});
+    fsMock.isDir.mockReturnValueOnce(false);
+
+    expect(isMounted(fsMock as any, 'dummy', 'dir')).to.be.false;
+  });
+
+  it('should return false when mounted but not file', () => {
+    const fsMock = getFsMock();
+    fsMock.stat.mockReturnValueOnce({});
+    fsMock.isFile.mockReturnValueOnce(false);
+
+    expect(isMounted(fsMock as any, 'dummy', 'file')).to.be.false;
+  });
+});
---
+++ spec/path/mountBuffer-spec.ts
@@ -0,0 +1,56 @@
+import { expect } from 'chai';
+import * as nanoid from 'nanoid';
+import { isMounted } from '../../src/path/isMounted';
+import { mountBuffer } from '../../src/path/mountBuffer';
+
+jest.mock('nanoid');
+jest.mock('../../src/path/isMounted');
+
+const memPathId: string = 'memPathPrefixDummy';
+const getFsMock = () => ({
+  writeFile: jest.fn()
+});
+
+describe('mountBuffer', () => {
+  let fsMock: { writeFile: jest.Mock<any> };
+  let mountBufferFn: (contents: ArrayBufferView, fileName?: string) => string;
+
+  beforeEach(() => {
+    fsMock = getFsMock();
+    mountBufferFn = mountBuffer(fsMock as any, memPathId);
+  });
+
+  it('should return path if file is already mounted', () => {
+    (isMounted as jest.Mock<any>).mockReturnValueOnce(true);
+    const fileName = 'vFile.bin';
+
+    const value = mountBufferFn(new Buffer(''), fileName);
+
+    expect(value).to.equal(`${memPathId}/${fileName}`);
+  });
+
+  it('should write file in memory filesystem', () => {
+    (isMounted as jest.Mock<any>).mockReturnValueOnce(false);
+
+    const fileName = 'vFile.bin';
+    const contents = new Buffer('dummy');
+    const value = mountBufferFn(contents, fileName);
+    const expectedMountPath = `${memPathId}/${fileName}`;
+
+    expect(value).to.equal(expectedMountPath);
+    expect(fsMock.writeFile.mock.calls).to.have.lengthOf(1);
+    expect(fsMock.writeFile.mock.calls[0]).to.deep.equal([expectedMountPath, contents, { encoding: 'binary' }]);
+  });
+
+  it('should generate file name if not provided', () => {
+    const nanoIdMock = nanoid as jest.Mock<any>;
+    const dummyId = '_dummy_id';
+
+    nanoIdMock.mockReturnValueOnce(dummyId);
+
+    const value = mountBufferFn(new Buffer(''));
+
+    expect(value).to.equal(`${memPathId}/${dummyId}`);
+    expect(nanoIdMock.mock.calls).to.have.lengthOf(1);
+  });
+});
